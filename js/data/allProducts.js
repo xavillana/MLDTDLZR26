6391fac6bdca2a4d3d505ab52fb57094bb06cfa7
xavillana <161566442+xavillana@users.noreply.github.com>
2025-12-27 21:05:45 +0100

    Create allProducts.js

---
+++ js/data/allProducts.js
@@ -0,0 +1,9 @@
+import { cupcakes } from './cupcakes.js';
+import { cheesecakes } from './cheesecakes.js';
+import { tartas } from './tartas.js';
+
+export const allProducts = [
+    ...cupcakes,
+    ...cheesecakes,
+    ...tartas
+];
