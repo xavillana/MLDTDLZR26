67f9affba80eb57ec9e40bb442b3f0bad90a8e92
xavillana <161566442+xavillana@users.noreply.github.com>
2026-01-02 01:12:12 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,4 +3,6 @@ import { initMobileMenu, initModalSystem } from './core/ui.js';
 document.addEventListener("DOMContentLoaded", () => {
     initMobileMenu();
     initModalSystem();
+    openmodal();
 });
+
