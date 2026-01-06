2d83d0dcf4ccdf64eb3904e283cbfa9ac42ce511
xavillana <161566442+xavillana@users.noreply.github.com>
2026-01-06 23:58:50 +0100

    Delete js/main.js

--- js/main.js
+++
@@ -1,9 +0,0 @@
-import { initMobileMenu, initModalSystem, openModal } from './core/ui.js';
-
-document.addEventListener("DOMContentLoaded", () => {
-    initMobileMenu();
-    initModalSystem();
-    openModal();
-});
-
-
